fb483a673b89003df2c4ce3bfb0628fd3dcc81fb
MichaelGKing <michaelking.mgk@gmail.com>
2023-04-30 08:48:06 +0800

    c

--- frontend/src/components/Content.jsx
+++ frontend/src/components/Content.jsx
@@ -1,11 +1,16 @@
+import { Link } from 'react-router-dom';
+
 const Content = () => {
     return (
         <div className="text-center">
             <h2 className="text-4xl font-bold text-white">Please Select an Insurance Package</h2>
             <div className="flex justify-center mt-6">
-                <button className="w-32 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mx-auto">
-                    Rain
-                </button>
+                <Link to="../pages/rain.tsx">
+                    <button className="w-32 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mx-auto">
+                        Rain
+                    </button>
+                </Link>
+                {/* Add Link components for other buttons if needed */}
                 <button className="w-32 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mx-auto">
                     Drought
                 </button>
