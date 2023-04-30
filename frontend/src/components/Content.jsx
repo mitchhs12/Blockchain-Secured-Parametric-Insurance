828b9128f6d19eb081adf00a6d76fb3a8de22ad3
valodax <snitzel098@gmail.com>
2023-04-29 19:46:50 -0500

    :white_check_mark: `quicksave

--- frontend/src/components/Content.jsx
+++ frontend/src/components/Content.jsx
@@ -2,17 +2,17 @@ const Content = () => {
     return (
         <div className="text-center">
             <h2 className="text-4xl font-bold text-white">Please Select an Insurance Package</h2>
-            <div className="flex justify-center mt-6">
-                <button className="w-32 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mx-auto">
+            <div className="flex flex-wrap justify-center mt-6">
+                <button className="w-20 h-20 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mx-auto mb-4 sm:w-auto sm:h-auto">
                     Rain
                 </button>
-                <button className="w-32 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mx-auto">
+                <button className="w-20 h-20 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mx-auto mb-4 sm:w-auto sm:h-auto">
                     Drought
                 </button>
-                <button className="w-32 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mx-auto">
+                <button className="w-20 h-20 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mx-auto mb-4 sm:w-auto sm:h-auto">
                     Earthquake
                 </button>
-                <button className="w-32 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mx-auto">
+                <button className="w-20 h-20 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mx-auto mb-4 sm:w-auto sm:h-auto">
                     Snow
                 </button>
             </div>
