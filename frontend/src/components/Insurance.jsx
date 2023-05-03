b18532b56f1dd1aade67f3827b7f6fd68df9edc1
Michael King <michaelking.mgk@gmail.com>
2023-05-03 23:37:42 +0800

    c

--- frontend/src/components/Insurance.jsx
+++ frontend/src/components/Insurance.jsx
@@ -1,33 +1,41 @@
+import { useState } from 'react';
 import ContractInput from "@/components/ContractInput";
 import Map from "@/components/Map";
 
 const Insurance = ({ type }) => {
-    const configLabel =
-        type === "Rain"
-            ? "Quantity of Rainfall (mm)"
-            : type === "Earthquake"
-            ? "Magnitude of Earthquake (Richter scale)"
-            : type === "Snow"
-            ? "Quantity of Snowfall (cm)"
-            : type === "Drought"
-            ? "Lack of Soil moisture"
-            : "";
+  const configLabel =
+    type === "Rain"
+      ? "Quantity of Rainfall (mm)"
+      : type === "Earthquake"
+      ? "Magnitude of Earthquake (Richter scale)"
+      : type === "Snow"
+      ? "Quantity of Snowfall (cm)"
+      : type === "Drought"
+      ? "Lack of Soil moisture"
+      : "";
 
-    return (
-        <div className="flex flex-col items-center">
-            <div className="text-white font-bold text-4xl pb-8">{type} Insurance</div>
-            <div className="w-full flex flex-col lg:flex-row lg:items-center lg:justify-center lg:pb-4">
-                <div className="w-full lg:pr-4 xl:pr-8">
-                    <div className="h-96 w-full lg:h-auto border-2 max-w-2xl mx-auto lg:max-w-lg">
-                        <Map />
-                    </div>
-                </div>
-                <div className="w-full lg:pl-4 xl:pl-8 pt-10 pb-20">
-                    <ContractInput configLabel={configLabel} />
-                </div>
-            </div>
+  const [rectangleBounds, setRectangleBounds] = useState(null);
+
+  const handleRectangleBoundsChange = bounds => {
+    setRectangleBounds(bounds);
+  };
+
+  return (
+    <div className="flex flex-col items-center">
+      <div className="text-white font-bold text-4xl pb-8">{type} Insurance</div>
+      <div className="w-full flex flex-col lg:flex-row lg:items-center lg:justify-center lg:pb-4">
+        <div className="w-full lg:pr-4 xl:pr-8">
+          <div className="h-96 w-full lg:h-auto border-2 max-w-2xl mx-auto lg:max-w-lg">
+            <Map onRectangleBoundsChange={handleRectangleBoundsChange} />
+          </div>
         </div>
-    );
+        <div className="w-full lg:pl-4 xl:pl-8 pt-10 pb-20">
+          <ContractInput configLabel={configLabel} rectangleBounds={rectangleBounds} />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Insurance;
+
--- frontend/src/components/Map.jsx
+++ frontend/src/components/Map.jsx
@@ -1,87 +1,72 @@
-import React, { useState, useEffect } from "react";
-import { GoogleMap, LoadScriptNext, DrawingManager, StandaloneSearchBox } from "@react-google-maps/api";
+import React, { useState } from 'react';
+import { GoogleMap, LoadScript, DrawingManagerF, useLoadScript } from '@react-google-maps/api';
+import ContractInput from './ContractInput';
 
-function MyComponent() {
-    const [center, setCenter] = useState({ lat: 48.858, lng: 2.294 });
-    const [searchBox, setSearchBox] = useState(null);
-    const [containerStyle, setContainerStyle] = useState({
-        width: "380px",
-        height: "380px",
-    });
+const mapContainerStyle = {
+  height: '400px',
+  width: '400px'
+};
 
-    const handleSearchBoxLoad = (ref) => {
-        setSearchBox(ref);
-    };
+const center = {
+  lat: 38.685,
+  lng: -115.234
+};
 
-    const handlePlacesChanged = () => {
-        const places = searchBox.getPlaces();
-        if (places && places.length > 0) {
-            const { lat, lng } = places[0].geometry.location;
-            setCenter({ lat: lat(), lng: lng() });
-        }
-    };
+function MyMap() {
+  const [rectangleBounds, setRectangleBounds] = useState(null);
+  const { isLoaded } = useLoadScript({
+    googleMapsApiKey: "AIzaSyBLOuQAyQQbIrn9OqW3aWF0y574rj8MCsA",
+    libraries: ["drawing"],
+  });
 
-    const options = {
-        streetViewControl: false,
-    };
+  const onRectangleComplete = rectangle => {
+    const bounds = rectangle.getBounds();
+    const ne = bounds.getNorthEast();
+    const sw = bounds.getSouthWest();
+    const coordinates = [
+      { lat: ne.lat(), lng: ne.lng() },
+      { lat: sw.lat(), lng: ne.lng() },
+      { lat: sw.lat(), lng: sw.lng() },
+      { lat: ne.lat(), lng: sw.lng() }
+    ];
+    setRectangleBounds(coordinates);
+    console.log('Rectangle Bounds:', coordinates);
+  };
 
-    useEffect(() => {
-        const handleResize = () => {
-            if (window.innerWidth >= 1024) {
-                setContainerStyle({
-                    width: "700px",
-                    height: "700px",
-                });
-            } else {
-                setContainerStyle({
-                    width: "380px",
-                    height: "380px",
-                });
-            }
-        };
-
-        handleResize();
-
-        window.addEventListener("resize", handleResize);
-
-        return () => {
-            window.removeEventListener("resize", handleResize);
-        };
-    }, []);
-
-    return (
-        <div className="flex items-center justify-center">
-            <LoadScriptNext
-                googleMapsApiKey="AIzaSyBZuw3-0hopzdLU8nO3MtpXIW62LoNkfr4" // use process.env
-                libraries={["drawing", "places"]}
-            >
-                <GoogleMap mapContainerStyle={containerStyle} center={center} zoom={15} options={options}>
-                    <StandaloneSearchBox onLoad={handleSearchBoxLoad} onPlacesChanged={handlePlacesChanged}>
-                        <input
-                            type="text"
-                            placeholder="Search for a place..."
-                            style={{
-                                boxSizing: `border-box`,
-                                border: `1px solid transparent`,
-                                width: `240px`,
-                                height: `32px`,
-                                padding: `0 12px`,
-                                borderRadius: `3px`,
-                                boxShadow: `0 2px 6px rgba(0, 0, 0, 0.3)`,
-                                fontSize: `14px`,
-                                outline: `none`,
-                                textOverflow: `ellipses`,
-                                position: "absolute",
-                                bottom: "30px",
-                                left: "50%",
-                                marginLeft: "-120px",
-                            }}
-                        />
-                    </StandaloneSearchBox>
-                </GoogleMap>
-            </LoadScriptNext>
-        </div>
-    );
+  return (
+    <div>
+      {isLoaded && (
+        <GoogleMap
+          mapContainerStyle={mapContainerStyle}
+          zoom={10}
+          center={center}
+        >
+          <DrawingManagerF
+            onLoad={drawingManager => console.log(drawingManager)}
+            onRectangleComplete={onRectangleComplete}
+            options={{
+              drawingControl: true,
+              drawingControlOptions: {
+                position: window.google.maps.ControlPosition.TOP_CENTER,
+                drawingModes: [window.google.maps.drawing.OverlayType.RECTANGLE]
+              },
+              rectangleOptions: {
+                fillColor: '#FFFF00',
+                fillOpacity: 0.5,
+                strokeWeight: 2,
+                strokeColor: '#FFFF00',
+                clickable: true,
+                editable: true,
+                draggable: true,
+                zIndex: 1
+              }
+            }}
+          />
+        </GoogleMap>
+      )}
+      <ContractInput rectangleBounds={rectangleBounds} />
+    </div>
+  );
 }
 
-export default React.memo(MyComponent);
+export default React.memo(MyMap);
