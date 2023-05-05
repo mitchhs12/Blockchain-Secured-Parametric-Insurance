9d5bfd4e4206781ca3ef80744826d2016941645d
valodax <snitzel098@gmail.com>
2023-05-04 23:10:38 -0500

    :square: `center` center of square added

--- frontend/src/components/ContractInput.jsx
+++ frontend/src/components/ContractInput.jsx
@@ -1,55 +1,60 @@
 const ContractInput = ({ configLabel, rectangleBounds }) => {
-    const coordinates = rectangleBounds
-      ? rectangleBounds.map(coord => `${coord.lat},${coord.lng}`).join(';')
-      : '';
-  
+    let center = "";
+    if (rectangleBounds) {
+        const latSum = rectangleBounds.reduce((sum, corner) => sum + corner.lat, 0);
+        const lngSum = rectangleBounds.reduce((sum, corner) => sum + corner.lng, 0);
+        const centerLat = latSum / 4;
+        const centerLng = lngSum / 4;
+        center = { lat: centerLat, lng: centerLng };
+        console.log(center);
+    }
+
     return (
-      <div className="border-2 text-white font-bold">
-        Please draw a square on the map to select the area of interest.
-        <div className="mt-6 mb-6 flex flex-row items-center">
-          <div>
-            <input
-              id="coordinates"
-              type="text"
-              className="w-80 border-gray-300 rounded-lg py-2 sm:px-2 sm:py-1 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:border-transparent"
-              placeholder="Coordinates (Latitudes, Longitude)"
-              value={coordinates}
-              readOnly
-              style={{ color: 'black' }}
-            />
-          </div>
+        <div className="border-2 text-white font-bold">
+            Please draw a square on the map to select the area of interest.
+            <div className="mt-6 mb-6 flex flex-row items-center">
+                <div>
+                    <input
+                        id="coordinates"
+                        type="text"
+                        className="w-96 border-gray-300 rounded-lg py-2 sm:px-2 sm:py-1 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:border-transparent"
+                        placeholder="Square Center Coordinates (Latitude, Longitude)"
+                        value={center ? `${center.lat}, ${center.lng}` : ""}
+                        readOnly
+                        style={{ color: "black" }}
+                    />
+                </div>
+            </div>
+            <div className="mb-6 flex flex-row items-center">
+                <div>
+                    <input
+                        id="config"
+                        type="number"
+                        className="w-80 border-gray-300 rounded-lg py-2 sm:px-2 sm:py-1 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:border-transparent"
+                        placeholder={configLabel}
+                        min="1"
+                    />
+                </div>
+            </div>
+            <div className="mb-6 flex flex-row items-center">
+                <div>
+                    <input
+                        id="days"
+                        type="number"
+                        className="w-50 border-gray-300 rounded-lg px-4 py-2 sm:px-2 sm:py-1 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:border-transparent"
+                        placeholder="Number of Days"
+                        min="1"
+                    />
+                </div>
+            </div>
+            <div className="mb-6 flex flex-row items-center">
+                <div>Total Land Area Selected</div>
+            </div>
+            <div className="mb-6 flex flex-row items-center">
+                <div>Cost of Insurance</div>
+            </div>
         </div>
-        <div className="mb-6 flex flex-row items-center">
-          <div>
-            <input
-              id="config"
-              type="number"
-              className="w-80 border-gray-300 rounded-lg py-2 sm:px-2 sm:py-1 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:border-transparent"
-              placeholder={configLabel}
-              min="1"
-            />
-          </div>
-        </div>
-        <div className="mb-6 flex flex-row items-center">
-          <div>
-            <input
-              id="days"
-              type="number"
-              className="w-50 border-gray-300 rounded-lg px-4 py-2 sm:px-2 sm:py-1 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:border-transparent"
-              placeholder="Number of Days"
-              min="1"
-            />
-          </div>
-        </div>
-        <div className="mb-6 flex flex-row items-center">
-          <div>Total Land Area Selected</div>
-        </div>
-        <div className="mb-6 flex flex-row items-center">
-          <div>Cost of Insurance</div>
-        </div>
-      </div>
     );
-  };
-  
-  export default ContractInput;
-  
+};
+
+export default ContractInput;
--- frontend/src/components/Map.jsx
+++ frontend/src/components/Map.jsx
@@ -1,80 +1,75 @@
-import React, { useState } from 'react';
-import { GoogleMap, LoadScript, DrawingManagerF, useLoadScript } from '@react-google-maps/api';
-import ContractInput from './ContractInput';
+import React, { useState } from "react";
+import { GoogleMap, LoadScript, DrawingManagerF, useLoadScript } from "@react-google-maps/api";
 
 const mapContainerStyle = {
-  height: '400px',
-  width: '400px'
+    height: "400px",
+    width: "400px",
 };
 
 const center = {
-  lat: 38.685,
-  lng: -115.234
+    lat: 38.685,
+    lng: -115.234,
 };
 
-function MyMap({changeRectangle}) {
- 
-  const { isLoaded } = useLoadScript({
-    googleMapsApiKey: "AIzaSyBLOuQAyQQbIrn9OqW3aWF0y574rj8MCsA",
-    libraries: ["drawing"],
-  });
+function MyMap({ changeRectangle }) {
+    const { isLoaded } = useLoadScript({
+        googleMapsApiKey: "AIzaSyBLOuQAyQQbIrn9OqW3aWF0y574rj8MCsA",
+        libraries: ["drawing"],
+    });
 
-  const [currentRectangle, setCurrentRectangle] = useState(null);
+    const [currentRectangle, setCurrentRectangle] = useState(null);
 
-  const onOverlayComplete = overlay => {
-    if (currentRectangle) {
-      currentRectangle.setMap(null);
-    }
+    const onOverlayComplete = (overlay) => {
+        if (currentRectangle) {
+            currentRectangle.setMap(null);
+        }
 
-    const rectangle = overlay.overlay;
-    setCurrentRectangle(rectangle);
+        const rectangle = overlay.overlay;
+        setCurrentRectangle(rectangle);
 
-    const bounds = rectangle.getBounds();
-    const ne = bounds.getNorthEast();
-    const sw = bounds.getSouthWest();
-    const coordinates = [
-      { lat: ne.lat(), lng: ne.lng() },
-      { lat: sw.lat(), lng: ne.lng() },
-      { lat: sw.lat(), lng: sw.lng() },
-      { lat: ne.lat(), lng: sw.lng() }
-    ];
-    changeRectangle(coordinates);
-    console.log('Rectangle Bounds:', coordinates);
-  };
+        const bounds = rectangle.getBounds();
+        const ne = bounds.getNorthEast();
+        const sw = bounds.getSouthWest();
 
-  return (
-    <div>
-      {isLoaded && (
-        <GoogleMap
-          mapContainerStyle={mapContainerStyle}
-          zoom={10}
-          center={center}
-        >
-          <DrawingManagerF
-            onLoad={drawingManager => console.log(drawingManager)}
-            onOverlayComplete={onOverlayComplete}
-            options={{
-              drawingControl: true,
-              drawingControlOptions: {
-                position: window.google.maps.ControlPosition.TOP_CENTER,
-                drawingModes: [window.google.maps.drawing.OverlayType.RECTANGLE]
-              },
-              rectangleOptions: {
-                fillColor: '#FFFF00',
-                fillOpacity: 0.5,
-                strokeWeight: 2,
-                strokeColor: '#FFFF00',
-                clickable: true,
-                editable: true,
-                draggable: true,
-                zIndex: 1
-              }
-            }}
-          />
-        </GoogleMap>
-      )}
-    </div>
-  );
+        const coordinates = [
+            { lat: ne.lat(), lng: ne.lng() }, // top right of square
+            { lat: sw.lat(), lng: ne.lng() }, // bottom right of square
+            { lat: sw.lat(), lng: sw.lng() }, // bottom left of square
+            { lat: ne.lat(), lng: sw.lng() }, // top left of square
+        ];
+        changeRectangle(coordinates);
+        console.log("Rectangle Bounds:", coordinates);
+    };
+
+    return (
+        <div>
+            {isLoaded && (
+                <GoogleMap mapContainerStyle={mapContainerStyle} zoom={10} center={center}>
+                    <DrawingManagerF
+                        onLoad={(drawingManager) => console.log(drawingManager)}
+                        onOverlayComplete={onOverlayComplete}
+                        options={{
+                            drawingControl: true,
+                            drawingControlOptions: {
+                                position: window.google.maps.ControlPosition.TOP_CENTER,
+                                drawingModes: [window.google.maps.drawing.OverlayType.RECTANGLE],
+                            },
+                            rectangleOptions: {
+                                fillColor: "#FFFF00",
+                                fillOpacity: 0.5,
+                                strokeWeight: 2,
+                                strokeColor: "#FFFF00",
+                                clickable: true,
+                                editable: true,
+                                draggable: true,
+                                zIndex: 1,
+                            },
+                        }}
+                    />
+                </GoogleMap>
+            )}
+        </div>
+    );
 }
 
 export default React.memo(MyMap);
