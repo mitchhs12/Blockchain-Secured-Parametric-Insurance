4eb31ce3fd8a4e94d9cede410c5ac6c5f6c96386
valodax <snitzel098@gmail.com>
2023-05-23 15:02:31 -0500

    quicksaving

--- backend/contracts/Insurance.sol
+++ backend/contracts/Insurance.sol
@@ -1,20 +1,39 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.4;
 
 import "./insurances/Rain.sol";
 import "./insurances/Drought.sol";
 import "./insurances/Earthquake.sol";
 import "./insurances/Snow.sol";
+import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+import {Functions, FunctionsClient} from "./dev/functions/FunctionsClient.sol";
+import {ConfirmedOwner} from "@chainlink/contracts/src/v0.8/ConfirmedOwner.sol";
 
-contract InsuranceContract {
-  enum InsuranceType {
-    Rain, // 0
-    Drought, // 1
-    Earthquake, // 2
-    Snow // 3
+contract Insurance is FunctionsClient, ConfirmedOwner {
+  using Functions for Functions.Request;
+
+  bytes32 public latestRequestId;
+  bytes public latestResponse;
+  bytes public latestError;
+
+  event OCRResponse(bytes32 indexed requestId, bytes result, bytes err);
+
+  AggregatorV3Interface internal priceFeed;
+
+  constructor(address oracle) FunctionsClient(oracle) ConfirmedOwner(msg.sender) {
+    priceFeed = AggregatorV3Interface(
+      "0x12162c3E810393dEC01362aBf156D7ecf6159528" //Polygon Mumbai LINK/MATIC pricefeed
+    );
   }
 
-  struct InsurancePolicy {
+  enum InsuranceType {
+    Rain,
+    Drought,
+    Earthquake,
+    Snow
+  }
+
+  struct InsuranceData {
     uint256 latNw;
     uint256 longNw;
     uint256 latNe;
@@ -26,11 +45,25 @@ contract InsuranceContract {
     uint256 configParam;
     InsuranceType insuranceType;
     uint256 numDays;
+    uint256 startDay;
   }
 
-  mapping(address => InsurancePolicy[]) public userPolicies;
+  mapping(address => InsuranceData) public insuranceMapping;
 
-  function purchaseInsurance(
+  function getLatestPrice() public view returns (int) {
+    (
+      ,
+      /* uint80 roundID */ int price /*uint startedAt*/ /*uint timeStamp*/ /*uint80 answeredInRound*/,
+      ,
+      ,
+
+    ) = priceFeed.latestRoundData();
+    return price;
+  }
+
+  function estimateInsurance(
+    string calldata source,
+    bytes calldata secrets,
     uint256 latNw,
     uint256 longNw,
     uint256 latNe,
@@ -41,31 +74,52 @@ contract InsuranceContract {
     uint256 longSw,
     uint256 configParam,
     uint256 insuranceType,
-    uint256 numDays
+    uint256 numDays,
+    uint256 startDay
   ) public payable {
-    // Calculate insurance cost
-    uint256 cost = estimateInsuranceCost(configParam, insuranceType, numDays);
-    require(msg.value >= cost, "Insufficient funds");
-
-    // Create and append new insurance policy to user mapping
-    InsurancePolicy memory newPolicy = InsurancePolicy(
-      latNw,
-      longNw,
-      latNe,
-      longNe,
-      latSe,
-      longSe,
-      latSw,
-      longSw,
-      configParam,
-      insuranceType,
-      numDays
-    );
-    userPolicies[msg.sender].push(newPolicy);
-
-    // Send any excess funds back to user
-    if (msg.value > cost) {
-      payable(msg.sender).transfer(msg.value - cost);
+    // Ensure at least 1 Matic is sent
+    require(msg.value >= 1 ether, "Not enough Matic sent");
+    InsuranceData storage newInsuranceData = InsuranceData({
+      latNw: latNw,
+      longNw: longNw,
+      latNe: latNe,
+      longNe: longNe,
+      latSe: latSe,
+      longSe: longSe,
+      latSw: latSw,
+      longSw: longSw,
+      configParam: configParam,
+      insuranceType: insuranceType,
+      numDays: numDays,
+      startDay: startDay
+    });
+    Functrions.Request memory req;
+    req.initializeRequest(Functions.Location.Inline, Functions.CodeLanguage.JavaScript, source);
+    if (secrets.length > 0) {
+      req.addRemoteSecrets(secrets);
     }
+    if (args.Length > 0) req.addArgs(args);
+    // Calls Chainlink DON for insurance cost
+    insuranceMapping[msg.sender] = newInsuranceData;
+  }
+
+  function checkInsuranceData() public payable returns (uint256) {
+    require(
+      InsuranceData.latNw != 0 &&
+        InsuranceData.longNw != 0 &&
+        InsuranceData.latNe != 0 &&
+        InsuranceData.longNe != 0 &&
+        InsuranceData.latSe != 0 &&
+        InsuranceData.longSe != 0 &&
+        InsuranceData.latSw != 0 &&
+        InsuranceData.longSw != 0 &&
+        InsuranceData.configParam != 0 &&
+        InsuranceData.insuranceType != 0 &&
+        InsuranceData.numDays != 0 &&
+        InsuranceData.startDay != 0,
+      "User data not found or incomplete"
+    );
+
+    return 1;
   }
 }
--- frontend/src/insurance_estimators/rainfall.jsx
+++ frontend/src/insurance_estimators/rainfall.jsx
@@ -1,4 +1,4 @@
-const getKoppenGeigerZones = require("../utils/getKoppenGeigerZones");
+const getKoppenGeigerZones = require("@/utils/getKoppenGeigerZones");
 const getAverageRainfall = require("../insurance_estimators/getAverageRainfall");
 
 function sinCurveNothern(x) {
@@ -45,36 +45,4 @@ export function estimateRainfall(rectangleBounds, area, dateRange, aboveOrBelow,
         .catch((err) => {
             console.error(err);
         });
-
-    // Af (Tropical rainforest): 30 (2000-3000mm/y)
-    // Am (Tropical monsoon): 29 (1800-2500mm/y)
-    // As (Tropical savanna, dry summer): 28 (700-1300mm/y)
-    // Aw (Tropical savanna, dry winter): 27 (700-1300mm/y)
-    // BWh (Desert, hot): 1 (<250mm/y)
-    // BWk (Desert, cold): 2 (<250mm/y)
-    // BSh (Steppe, hot): 5 (250-500mm/y)
-    // BSk (Steppe, cold): 6 (250-500mm/y)
-    // Csa (Mediterranean, hot summer): 12 (400-800mm/y)
-    // Csb (Mediterranean, warm summer): 13 (500-900/y)
-    // Csc (Mediterranean, cool summer): 14 (600-1000/y)
-    // Cwa (Humid subtropical, dry winter): 26 (800-1400mm/y)
-    // Cwb (Subtropical highland, dry winter): 25 (800-1200mm/y)
-    // Cwc (Subtropical highland, very dry winter): 24 (800-1200mm/y)
-    // Cfa (Humid subtropical, no dry season): 28 (1000-2000mm/y)
-    // Cfb (Marine, warm summer): 22 (800-1400mm/y)
-    // Cfc (Marine, cool summer): 21 (800-1400mm/y)
-    // Dsa (Hot-summer humid continental): 17 (400-800mm/y)
-    // Dsb (Warm-summer humid continental): 18 (500-900mm/y)
-    // Dsc (Cool-summer humid continental): 19 (600-1000mm/y)
-    // Dsd (Continental subarctic, extremely dry winter): 20 (500-900mm/y)
-    // Dwa (Humid continental, dry winter, hot summer): 16 (600-1000mm/y)
-    // Dwb (Humid continental, dry winter, warm summer): 15 (700-1100mm/y)
-    // Dwc (Humid continental, dry winter, cool summer): 14 (700-1100mm/y)
-    // Dwd (Humid continental, dry winter, extremely dry winter): 13 (600-1200mm/y)
-    // Dfa (Humid continental, no dry season, hot summer): 23 (700-1200mm/y)
-    // Dfb (Humid continental, no dry season, warm summer): 24 (500-900mm/y)
-    // Dfc (Subarctic, no dry season, cool summer): 11 (400-800mm/y)
-    // Dfd (Subarctic, no dry season, extremely cold winter): 10 (400-800mm/y)
-    // ET (Tundra): 8 (200-600mm/y)
-    // EF (Ice cap): 7 (<200mm/y)
 }
